b771e5ce9a02eb990386f8bc1c22053bc6f1898a
miuratomonori <112144367+miuratomonori@users.noreply.github.com>
2022-08-28 14:47:13 +0900

    Add files via upload

---
+++ answer_3.php
@@ -0,0 +1,56 @@
+<?php
+
+        $ancer=$_POST['ancer'];
+        $id=$_COOKIE["id"];
+
+        if(strcmp($ancer,"sadogasima")==0){
+                $dsn='mysql:dbname=scoresever;host=localhost';
+                $user='ncc_ctf';
+                $password='password';
+                $dbh=new PDO($dsn,$user,$password);
+
+                $sql = 'update point set point = point + 1 where user_id = ?';
+                $stmt=$dbh->prepare($sql);
+                $flag=$stmt->execute(array($id));
+
+                if($flag){
+                        $put = "正解";
+                }else{
+                        $put = 'error';
+                }
+        }else{
+                $put = "不正解!";
+        }
+        echo "<h1 class=result>";
+                echo $put;
+        echo "</h1>";
+
+?>
+
+<!DOCTYPE html>
+<head>
+	<meta chraset="utf-8">
+	<link rel="stylesheet" href="aiueo.css">
+	<script type="text/ecmascript">
+    		function HeaderClick() {
+      		target = document.getElementById("ContentsPanel");
+      			if (target.style.display == "none") {
+        			target.style.display = "block";
+      			} else {
+        			target.style.display = "none";
+      			}
+   		 }
+  	</script>
+</head>
+
+<body>
+
+	
+	<p id="ContentsHeader" onclick="HeaderClick();">解説</p>
+	<div id="ContentsPanel">Stirling.exeファイルをドラックするとヒントの数字が出てきます。<br>
+	それを検索し出てきたサイトを見ると、3つ拡張子が出てくるのでそれをファイルの後ろにつけるとQRコード	  が出てきます。<br>
+	読み取ると答えであるフラグが出てきます</div>
+	<p><a href="select.php">問題画面に戻る</a></p>
+
+</body>
+</html>
