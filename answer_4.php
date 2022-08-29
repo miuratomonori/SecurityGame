c3ca087af7ec81bf361846de19a611e5781a5391
miuratomonori <112144367+miuratomonori@users.noreply.github.com>
2022-08-29 14:42:54 +0900

    Update answer_4.php

--- answer_4.php
+++ answer_4.php
@@ -33,8 +33,8 @@
 
 	
 	<p id="ContentsHeader" onclick="HeaderClick();">解説</p>
-	<div id="ContentsPanel">メモ帳の機能で並んでいる「java」の文字をすべて置き換えます。残った文字の中に３つずつ並んでいる大文字があります。<br>
-	これをつなげると「FLAG IS DRIVE」となるため答えはDRIVEとなります</div>
+	<div id="ContentsPanel">メモ帳の機能で並んでいる「java」の文字をすべて置き換えます。残った文字の中に３つずつ並んでいる大文字を並べると<br>
+	「FLAG IS DRIVE」となるため答えはDRIVEとなります</div>
 	<p><a href="select.php">問題画面に戻る</a></p>
 
 </body>
