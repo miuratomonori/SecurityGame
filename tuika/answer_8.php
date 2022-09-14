c1390aeb41e8ae5f046d85dbad38767585326ce5
miuratomonori <112144367+miuratomonori@users.noreply.github.com>
2022-09-14 15:26:39 +0900

    Add files via upload

---
+++ tuika/answer_8.php
@@ -0,0 +1,54 @@
+<?php
+
+	$ancer=$_POST['ancer'];	
+	$id=$_COOKIE["id"];
+
+	if(strcmp($ancer,"2610121518212531475571727983849499111459")==0){
+		$dsn='mysql:dbname=scoresever;host=localhost';
+        $user='ncc_ctf';
+        $password='password';
+        $dbh=new PDO($dsn,$user,$password);
+
+		$sql = 'update point set point = point + 1 where user_id = ?';
+		$stmt=$dbh->prepare($sql);
+		$flag=$stmt->execute(array($id));  
+		
+		if($flag){
+			$put = "正解";
+		}else{
+			$put = '正解！※データベース更新失敗';
+		}
+	}else{
+		$put = "不正解!";
+	}
+	echo "<h1 class=result>";
+		echo $put;
+	echo "</h1>";
+
+?>
+
+<!DOCTYPE html>
+<head>
+	<meta chraset="utf-8">
+	<link rel="stylesheet" href="aiueo.css">
+	<script type="text/ecmascript">
+    		function HeaderClick() {
+      		target = document.getElementById("ContentsPanel");
+      			if (target.style.display == "none") {
+        			target.style.display = "block";
+      			} else {
+        			target.style.display = "none";
+      			}
+   		 }
+  	</script>
+</head>
+
+<body>
+
+	
+	<p id="ContentsHeader" onclick="HeaderClick();">解説</p>
+	<div id="ContentsPanel">miniwebtool.comを使用すると簡単に昇順にすることができます</div>
+	<p><a href="select.php">問題画面に戻る</a></p>
+
+</body>
+</html>
